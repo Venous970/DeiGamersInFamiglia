4505e7dec4a0853098f74734868ca11003f97ce7
Venous970 <nicholasbeghetto991@gmail.com>
2024-05-18 20:53:03 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 8080; // Cambia la porta qui
+
+const server = http.createServer((req, res) => {
+    const filePath = req.url === '/' ? 'index.html' : req.url;
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(path.join(__dirname, filePath), (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end('Server Error: ' + error.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
